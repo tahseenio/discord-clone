75a347b0a752f1e4037c12f8381c6bd6e8e0e75d
Tahseen <tahseenislam@hotmail.com>
2022-03-15 09:53:16 +1000

    added react router 6

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import Logo from '../assets/discord-logo.svg'
-
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <>
     <nav>
-      <a href="#">
-        <img className='nav-logo' src={Logo} alt="" />
-      </a>
-      <div className="nav__links">
-        <a className="nav__link" href="#">Download</a>
-        <a className="nav__link" href="#">Nitro</a>
-        <a className="nav__link" href="#">Safety</a>
-        <a className="nav__link" href="#">Support</a>
-        <a className="nav__link" href="#">Blog</a>
-        <a className="nav__link" href="#">Careers</a>
-      </div>
+      <Link to="/discord-clone">
+        <a href="#">
+          <img className='nav-logo' src={Logo} alt="" />
+        </a>
+      </Link>
+      <ul className="nav__links">
+        <Link to="/discord-clone/download" className="nav__link">
+          Download
+        </Link>
+        <Link to="/discord-clone/nitro" className="nav__link">
+          Nitro
+        </Link>
+        <Link to="/discord-clone/safety" className="nav__link">
+          Safety
+        </Link>
+        <Link to="/discord-clone/support" className="nav__link">
+          Support
+        </Link>
+        <Link to="/discord-clone/blog" className="nav__link">
+          Blog
+        </Link>
+        <Link to="/discord-clone/careers" className="nav__link">
+          Careers
+        </Link>
+      </ul>
       <a className="btn--login no-cursor" href="#">Login</a>
     </nav>
-    
-    </>
-
   )
 }
---
+++ src/pages/DownloadPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Devices from '../components/Devices'
+
+export default function DownloadPage() {
+  return (
+    <div>
+      <Devices />
+    </div>
+  )
+}
